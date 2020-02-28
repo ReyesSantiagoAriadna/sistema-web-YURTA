960f5bd956961d4bf42c03dde8a69bdb37d88a19
Azaemorales2329 <azaelmorales029@gmail.com>
2020-02-28 12:06:19 -0600

    firebase

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -180,3 +180,14 @@ class ApiController extends Controller
                 ->get();
  *
  * */
+
+
+/*rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if false;
+    }
+  }
+}
+ * */
