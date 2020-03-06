73b2667420bdd6b0508aa62c27b45e0ebe45d0dc
Azaemorales2329 <azaelmorales029@gmail.com>
2020-03-06 11:33:54 -0600

    push-notification

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -129,9 +129,9 @@ class PedidosController extends Controller
             ->join('users','users.id','=','obra.encargado')
             ->select('users.fcm_token')->get();
 
-        NotificacionController::sendPushNotification($result,"Pedido confirmado"
-            ,"Tu pedido se ha confrimado va en camino");
-        return $this->mostrar();
+        return $this->sendPushNotification($result,"Pedido confirmado"
+            ,"Tu pedido se ha confirmado va en camino");
+        //return $this->mostrar();
     }
 
     function disminuir_existencias($id,$cantidad){ 
@@ -179,7 +179,45 @@ class PedidosController extends Controller
             }
         }   
  
-}
+    }
+
+    public  function sendPushNotification($fcm_token,$title,$message) {
+        $id=null;
+        $API_ACCESS_KEY="AIzaSyAUNcqTUttLsiBm9YCs344VbI5Wcil6BZ0";
+        $url = "https://fcm.googleapis.com/fcm/send";
+        $header = [
+            'authorization: key=' . $API_ACCESS_KEY,
+            'content-type: application/json'
+        ];
+
+        $postdata = '{
+            "to" : "' . $fcm_token . '",
+                "notification" : {
+                    "title":"' . $title . '",
+                    "text" : "' . $message . '"
+                },
+            "data" : {
+                "id" : "'.$id.'",
+                "title":"' . $title . '",
+                "description" : "' . $message . '",
+                "text" : "' . $message . '",
+                "is_read": 0
+              }
+        }';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+    }
  
  
 } 
